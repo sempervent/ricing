22109ecdd816ec9436bd014aa9a443e389c7bc5c
sempervent <jngrant9@gmail.com>
2023-02-20 20:55:59 -0500

    Tonight's the night: Sleep in a eucalyptus tree.

--- ricing/db.py
+++ ricing/db.py
@@ -10,22 +10,34 @@ from ricing import RICE_DB
 
 
 class RiceDB(BaseModel):
-    """This RiceDB connects to ricing's sqlite database."""
+    """RiceDB connects to ricing's sqlite database."""
     conn_str: str = RICE_DB
     conn_fxn: Callable = connect2sqlite3
-    conn_kwargs: Optional[dict] = None
+    conn_kwargs: Optional[dict] = None  # TODO create models of specifics
     connection: Optional[Connection] = None
 
     def __enter__(self):
         """Use with to enter database conneciton."""
-        if self.conn_kwargs is None:
-            self.conn_kwargs = {}
-        self.connection = self.conn_fxn(self.conn_str, **self.conn_kwargs)
+        self.connect()
         return self
 
     def __exit__(self, _type, _value, _traceback):
-        """Safely exist the connection."""
+        """Safely exit the connection."""
+        self.close()
+
+    def close(self):
+        """Define a close method to end long running connection settings."""
         if hasattr(self.connection, 'close'):
             self.connection.close()
         self.connection = None
 
+    def connect(self):
+        """Safely connect to the database or keep a connection open."""
+        if self.conn_kwargs is None:
+            self.conn_kwargs = {}
+        if self.connection is None:
+            self.connection = self.conn_fxn(self.conn_str, **self.conn_kwargs)
+
+    def open(self):
+        """Open method to maintain a connection for long sessions."""
+        self.connect()
---
+++ ricing/sql.py
@@ -0,0 +1,63 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""Describe the ricing sql model."""
+from uuid import uuid4
+
+from sqlalchemy import (
+    Column, Integer, String, Table, create_engine, ForeignKey
+)
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship
+
+from ricing import RICE_CONFIG_DB
+
+Base = declarative_base()
+
+# create many-to-many for Config <=> File
+config_file_relations = Table(
+    'config_file_relations',
+    Base.metadata,
+    Column('config_id', Integer, ForeignKey('config.id')),
+    Column('file_id', String, ForeignKey('file.id')),
+)
+
+class Config(Base):
+    __tablename__ = 'config'
+    id = Column(Integer, primary_key=True)
+    name = Column(String)
+    files = relationship('File', secondary=config_file_relations)
+    programs = relationship('Program', back_populates='config')
+
+
+class File(Base):
+    __tablename__ = "file"
+    id = Column(String, primary_key=True, default=str(uuid4()))
+    hash = Column(String)
+    contents = Column(String)
+    path = Column(String)
+    name = Column(String)
+    programs = relationship('Program', back_populates='file')
+
+
+class Program(Base):
+    __tablename__ = "program"
+    id = Column(String, primary_key=True, default=str(uuid4()))
+    name = Column(String)
+    path = Column(String)
+    config_id = Column(Integer, ForeignKey('config.id'))
+    config = relationship('Config', back_populates='program')
+    file_id = Column(String, ForeignKey('file.id'))
+    file = relationship('File', back_populaties='program')
+
+
+def create_rice_db(connection_str: str | None, **kwargs):
+    """Create the rice database for storing configs.
+    Args:
+        connection_str: optional string for sqlalchemy to connect on
+        kwargs: optional arguments to create_engine
+    """
+    if connection_str is None:
+        connection_str = RICE_CONFIG_DB
+    engine = create_engine(connection_str, **kwargs)
+    Base.metadata.create_all(engine)
+
